d039059d5d57896b8a6dbe058681021bad54151a
SohamLimaye <soham.22111058@viit.ac.in>
2024-10-04 11:53:26 +0530

    Created homepage. created title and user greeting section

---
+++ dev/client/EnergySavr_Frontend/src/pages/Homepage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Homepage.css';
+
+const Homepage = () => {
+    return (
+        <div>
+            <div className="Title">
+                <div className="Rectangle73" />
+                <div className="EnergySavr">
+                    <span>Energy<br /></span>
+                    <span>Savr</span>
+                </div>
+            </div>
+            <div className="UserGreeting">
+                <div className="HiSoham">
+                    <span>Hi</span>
+                    <span> Soham!</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Homepage;
